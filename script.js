5afbd7161fce0053e3467b71c97f1f06347da3d9
Raj Dubal <126544333+0x1zen@users.noreply.github.com>
2026-01-21 06:17:07 +0000

    again

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ console.log("hello world"); //master branch
 
 console.log("hello world master conflict"); //master branch
 
+console.log("hello world master conflict again"); //master branch
+
 console.log("hello world dev1"); //added code in dev1 branch
 
